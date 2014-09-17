62cdd10c0b3f400ff5a2006159ab88dc5c879587
Michael Bastos <bastosmichael@gmail.com>
2014-09-17 06:46:31 -0700

    Temp removing results

--- app/models/results.rb
+++ app/models/results.rb
@@ -38,7 +38,7 @@ class Results < Url
             new_hash[date] = value
             if screenshot
               new_data['screenshot'][date] = screenshot
-              Screener.perform_async url, date
+              # Screener.perform_async url, date
             end
           end
         end
@@ -48,7 +48,7 @@ class Results < Url
         if screenshot
           if !new_data['screenshot']
             new_data['screenshot'] = { date => screenshot }
-            Screener.perform_async url, date
+            # Screener.perform_async url, date
           end
         end
       end
