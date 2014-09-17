00e780c25aa92a1fc3f141256e54658fa11d60b8
Michael Bastos <bastosmichael@gmail.com>
2014-09-17 06:13:25 -0700

    Adding results update

--- app/models/results.rb
+++ app/models/results.rb
@@ -1,21 +1,23 @@
 class Results < Url
-  CANONICAL = %i/site_name 
-                 id 
-                 url 
-                 type 
-                 date 
-                 name 
-                 image 
+  CANONICAL = %i/site_name
+                 id
+                 url
+                 type
+                 date
+                 name
+                 image
                  description/.freeze
 
   attr_accessor :metadata
+  attr_accessor :screenshot
 
   def sync
     self.data = update_metadata(update_canonical(data))
   end
 
   def update_canonical new_data = {}
-    date = metadata['date'] if date_exists?
+    set_date
+    set_screenshot
     metadata.each do |key, value|
       if CANONICAL.include? key.to_sym
         new_data[key] = value
@@ -34,40 +36,57 @@ class Results < Url
         original_hash.each do |k, v|
           if k == last_key && v != value
             new_hash[date] = value
+            if screenshot
+              new_data['screenshot'][date] = screenshot
+              Screener.perform_async url, date
+            end
           end
         end
         new_data[key] = original_hash.merge!(new_hash)
       else
         new_data[key] = {date => value}
+        if screenshot
+          if !new_data['screenshot']
+            new_data['screenshot'] = { date => screenshot }
+            Screener.perform_async url, date
+          end
+        end
       end
     end
     return new_data
   end
 
+  def set_date
+    self.date = metadata['date'] if metadata['date']
+  end
+
+  def set_screenshot
+    if metadata['screenshot']
+      self.screenshot = metadata['screenshot']
+      metadata.delete('screenshot')
+    end
+  end
+
   def data
     JSON.parse(cloud.get(json_relative_path).try(:body), :quirks_mode => true)
-  rescue 
+  rescue
     {}
   end
 
   def data= new_hash = {}
-  	cloud.sync json_relative_path, new_hash.to_json
+    cloud.sync json_relative_path, new_hash.to_json
   end
 
   def json_relative_path
-  	md5 + '.json'
+    md5 + '.json'
   end
 
   def cache_data
-  	File.join(host, types, md5)
+    File.join(host, types, md5)
   end
 
   def types
     metadata['type'].downcase.pluralize
-  end
-
-  def date_exists?
-    metadata['date']
   end
 
   def cloud
