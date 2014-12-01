7194bed471165549922c0e4597e65757ecef8ec4
Michael Bastos <bastosmichael@gmail.com>
2014-12-01 06:14:05 -0800

    Adding remote redis namespacing

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,3 +4,11 @@ require 'sidekiq/web'
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == %w(admin password)
 end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: "redis://#{SETTINGS[:redis][:address]}:#{SETTINGS[:redis][:port]}/#{SETTINGS[:redis][:database]}"}#, namespace: 'crawler' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://#{SETTINGS[:redis][:address]}:#{SETTINGS[:redis][:port]}/#{SETTINGS[:redis][:database]}"}#, namespace: 'crawler' }
+end
