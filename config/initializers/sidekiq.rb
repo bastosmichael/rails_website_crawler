c5d2b22169e8bc042edceb184dfda0bc875c2234
Michael Bastos <bastosmichael@gmail.com>
2014-12-01 07:01:36 -0800

    Fixing sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,9 +6,9 @@ Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{SETTINGS[:redis][:address]}:#{SETTINGS[:redis][:port]}/#{SETTINGS[:redis][:database]}"}, namespace: 'crawler' }
+  config.redis = { url: "redis://#{SETTINGS[:redis][:address]}:#{SETTINGS[:redis][:port]}/#{SETTINGS[:redis][:database]}", namespace: 'crawler' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{SETTINGS[:redis][:address]}:#{SETTINGS[:redis][:port]}/#{SETTINGS[:redis][:database]}"}, namespace: 'crawler' }
+  config.redis = { url: "redis://#{SETTINGS[:redis][:address]}:#{SETTINGS[:redis][:port]}/#{SETTINGS[:redis][:database]}", namespace: 'crawler' }
 end
