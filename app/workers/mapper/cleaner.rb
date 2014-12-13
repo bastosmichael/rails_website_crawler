9b0acdd96e389095f8a574fa3a8b01d09ed3213d
Michael Bastos <bastosmichael@gmail.com>
2014-12-12 19:05:28 -0800

    Working on cleaner

--- app/workers/mapper/cleaner.rb
+++ app/workers/mapper/cleaner.rb
@@ -1,19 +1,32 @@
 class Mapper::Cleaner < Mapper::Base
 
-  def perform container
+  def perform container, standard = []
     @container = container
     records.with_progress.each do |r|
       data = record(r.key).data
-      if id = data['id']
-        data.with_progress.each do |k,v|
-          ap k
-          if value = v.is_a?(Hash)
-            ap v.values
-          else
-            ap v
+      new_data = parse_record data
+      record(r.key).data = new_data unless data == new_data
+    end
+  end
+
+  def parse_record data
+    if id = data['id']
+      data.with_progress.each do |k,v|
+        ap 'KEY'
+        ap k
+        if v.is_a?(Hash)
+          v.each do |k2, v2|
+            ap '!!!!!!!!!!!!INNER KEY'
+            ap k2
+            ap '!!!!!!!!!!!!INNER VALUE'
+            ap v2
           end
+        else
+          ap 'VALUE'
+          ap v
         end
       end
     end
+    data
   end
 end
