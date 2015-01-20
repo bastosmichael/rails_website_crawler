6536f53fc3e927aff2f964ba13f10790e7850c30
Michael Bastos <bastosmichael@gmail.com>
2015-01-20 07:25:41 -0800

    Added usage tracking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_filter :restrict_access
+  after_filter :track_usage
   respond_to :json
 
   def index
@@ -8,15 +9,37 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def track_usage
+    if api_json['last_used'] == Date.today.to_s
+      api_json['api_usage'] = api_json['api_usage'] + 1
+    else
+      api_json['last_used'] = Date.today.to_s
+      api_json['api_usage'] = 0
+    end
+    api_record.data = api_json
+  end
+
   def check_partner(access_token)
     @api_key = access_token
-    return true if api_json['active'] == true
+    return true if api_json['active'] == true && api_usage <= api_usage_cap
   rescue
     return false
   end
 
+  def api_usage_cap
+    api_json['api_usage_cap']
+  end
+
+  def api_usage
+    api_json['api_usage']
+  end
+
   def api_json
-    @api_json ||= Record::Base.new('api-keys', api_key).data
+    @api_json ||= api_record.data
+  end
+
+  def api_record
+    @api_record ||= Record::Base.new('api-keys', api_key)
   end
 
   def api_key
