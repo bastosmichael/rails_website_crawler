cd5a224ea3b9a9f707a40bae4838adad27b97e4a
Michael Bastos <bastosmichael@gmail.com>
2015-01-20 08:06:00 -0800

    Working api tracking usage

--- app/controllers/v1/access_controller.rb
+++ app/controllers/v1/access_controller.rb
@@ -3,10 +3,10 @@ class V1::AccessController < ApplicationController
   private
 
   def track_usage
-    if api_json['last_used'] == Date.today.to_s
+    if api_json['api_last_used'] == Date.today.to_s
       api_json['api_daily_usage'] = api_json['api_daily_usage'] + 1
     else
-      api_json['last_used'] = Date.today.to_s
+      api_json['api_last_used'] = Date.today.to_s
       api_json['api_daily_usage'] = 0
     end
     api_record.data = api_json
@@ -14,14 +14,18 @@ class V1::AccessController < ApplicationController
 
   def check_partner(access_token)
     @api_key = access_token
-    if api_json['active'] == true && api_usage_cap.nil?
+    if api_active && api_usage_cap.nil?
       return true
-    elsif api_json['active'] == true && api_daily_usage < api_usage_cap
+    elsif api_active && api_usage_cap
       track_usage
-      return true
+      return true if api_active && api_daily_usage < api_usage_cap
     end
   rescue
     return false
+  end
+
+  def api_active
+    api_json['active'] == true
   end
 
   def api_usage_cap
