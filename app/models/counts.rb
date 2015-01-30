b9dfd8ef51d334c79866fdaf4789e588a2c94d24
Michael Bastos <bastosmichael@gmail.com>
2015-01-29 20:18:18 -0800

    updating counts

--- app/models/counts.rb
+++ app/models/counts.rb
@@ -7,23 +7,13 @@ class Counts
     @directories ||= storage.directories.map(&:key)
   end
 
-  def self.directories_count
-    @directories_count ||= Rails.cache.fetch(directories, expires_at: 12.hours) do
-      hash = {}
-      directories.map { |c| hash[c] = Cloud.new(c).count }
-      return hash
-    end
-  end
-
   def self.visible_directories
     @visible_directories ||= directories.map {|d| d unless d.include?('-screenshots') || d.include?('api-keys') }.compact
   end
 
   def self.visible_counts
-    @visible_counts ||= Rails.cache.fetch(visible_directories, expires_at: 12.hours) do
-      hash = {}
-      visible_directories.map { |c| hash[c] = Cloud.new(c).count }
-      return hash
-    end
+    hash = {}
+    visible_directories.map { |c| hash[c] = Record::Base.new(c, '_names.json').data.keys.count }
+    return hash
   end
 end
