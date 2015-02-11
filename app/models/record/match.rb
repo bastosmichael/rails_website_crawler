ccc653cc65f1725f24c85e583ee95392e4ac39dd
Michael Bastos <bastosmichael@gmail.com>
2015-02-11 08:49:54 -0800

    updating match

--- app/models/record/match.rb
+++ app/models/record/match.rb
@@ -1,7 +1,29 @@
 class Record::Match < Record::Base
+  require 'amatch'
+  include Amatch
+
   def initialize(container, metadata = 'name')
     @container = container
     @record = "_#{metadata.pluralize}.json"
+  end
+
+  def find search
+    @search = search
+    return best if key.nil?
+  end
+
+  def best
+    match_hash.each do |k,v|
+      value = @search.levenshtein_similar(v)
+      if value > max
+        max = value
+        key = k
+      end
+    end
+  end
+
+  def match_hash
+    @match_hash ||= data
   end
 
   def max
