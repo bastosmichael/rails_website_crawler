54e14546fd2df21f06205bdc4190df5424206632
Michael Bastos <bastosmichael@gmail.com>
2015-02-16 15:40:51 -0800

    Adding sitemapper alternate

---
+++ app/workers/crawler/sitemapper_alternate.rb
@@ -0,0 +1,32 @@
+class Crawler::SitemapperAlternate < Crawler::Base
+  sidekiq_options queue: :sitemapper_alternate,
+                  retry: true,
+                  backtrace: true,
+                  unique: true,
+                  unique_job_expiration: 24 * 60 * 60
+
+  def perform(url)
+    @url = url
+    get_xml
+    sitemap.site_links.with_progress.each { |u| get_page u } if sitemap.sites?
+    sitemap.index_links.with_progress.each { |u| get_sitemap u } if sitemap.indexes?
+  rescue Net::HTTP::Persistent::Error
+    Crawler::SitemapperAlternate.perform_async @url
+  end
+
+  def get_xml
+    sitemap.xml = scraper.get
+  end
+
+  def get_page(url)
+    Crawler::Scrimper.perform_async url
+  end
+
+  def get_sitemap(url)
+    Crawler::SitemapperAlternate.perform_async url
+  end
+
+  def sitemap
+    @sitemap ||= Crawl::Sitemap.new(@url)
+  end
+end
