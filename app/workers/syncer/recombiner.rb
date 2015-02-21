02c68f55c70993ba9fd454d2c5cac2ae2aac5fa0
Michael Bastos <bastosmichael@gmail.com>
2015-02-20 19:04:11 -0800

    bringing recombiner back

--- app/workers/syncer/recombiner.rb
+++ app/workers/syncer/recombiner.rb
@@ -4,24 +4,17 @@ class Syncer::Recombiner < Syncer::Base
                type
                screenshot).freeze
 
-  def perform(container, metadata = 'name')
+  def perform(container)
     @container = container
-    @metadata = "_#{metadata.pluralize}.json"
     records.with_progress.each do |r|
-      unless mapped_keys.include?(r.key)
-        data = record(r.key).data
-        if id = data['id']
-          data.with_progress.each do |k, v|
-            value = v.is_a?(Hash) ? v.values.last : v
-            launch_combiner(k, id, value) unless EXCLUDE.include? k.to_sym
-          end
+      data = record(r.key).data
+      if id = data['id']
+        data.with_progress.each do |k, v|
+          value = v.is_a?(Hash) ? v.values.last : v
+          launch_combiner(k, id, value) unless EXCLUDE.include? k.to_sym
         end
       end
     end
-  end
-
-  def mapped_keys
-    @mapped_keys ||= Record::Base.new(@container, @metadata).data.keys.map {|r| r + '.json' }
   end
 
   def launch_combiner(item, id, value)
