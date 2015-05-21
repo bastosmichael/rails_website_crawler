0d143af14243f1059f34f199038f0308c8664dd1
Michael Bastos <bastosmichael@gmail.com>
2015-05-21 07:40:43 -0700

    Adding environment to elasticsearch index name

--- app/workers/mapper/combiner.rb
+++ app/workers/mapper/combiner.rb
@@ -1,5 +1,5 @@
 class Mapper::Combiner < Mapper::Base
   def perform(container, type, id, value)
-    Elasticsearch::Model.client.index index: container, type: container, id: id, body: { type => value }
+    Elasticsearch::Model.client.index index: Rails.env + '-' + container, type: container, id: id, body: { type => value }
   end
 end
