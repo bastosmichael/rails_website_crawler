795f4ce9b16c8e7ac059fc7a9baae9e2bcaa5623
Michael Bastos <bastosmichael@gmail.com>
2015-05-22 21:23:04 -0700

    Sorting _source

--- app/workers/mapper/combiner.rb
+++ app/workers/mapper/combiner.rb
@@ -3,7 +3,7 @@ class Mapper::Combiner < Mapper::Base
     index = Rails.env + '-' + container
     if Elasticsearch::Model.client.exists? index: index, type: container, id: id
       source = Elasticsearch::Model.client.get_source index: index, type: container, id: id
-      Elasticsearch::Model.client.index index: index, type: container, id: id, body: source.merge({ type => value })
+      Elasticsearch::Model.client.index index: index, type: container, id: id, body: source.merge({ type => value }).sort.to_h
     else
       Elasticsearch::Model.client.index index: index, type: container, id: id, body: { type => value }
     end
