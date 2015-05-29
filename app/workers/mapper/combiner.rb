3437ca5c83de6f4968129b3a906be113417d8f01
Michael Bastos <bastosmichael@gmail.com>
2015-05-29 05:57:08 -0700

    Sorting hashes

--- app/workers/mapper/combiner.rb
+++ app/workers/mapper/combiner.rb
@@ -1,6 +1,6 @@
 class Mapper::Combiner < Mapper::Base
   def perform(container, id, hash = {})
     index = Rails.env + '-' + container
-    Elasticsearch::Model.client.index index: index, type: container, id: id, body: hash
+    Elasticsearch::Model.client.index index: index, type: container, id: id, body: hash.sort.to_h
   end
 end
