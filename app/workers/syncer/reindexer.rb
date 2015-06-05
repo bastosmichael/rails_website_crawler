a70a2684b028b6cc6b9c652ceef7054d017faf0d
Michael Bastos <bastosmichael@gmail.com>
2015-06-05 05:56:50 -0700

    Fixing reindexer

--- app/workers/syncer/reindexer.rb
+++ app/workers/syncer/reindexer.rb
@@ -2,6 +2,7 @@ class Syncer::Reindexer < Syncer::Base
   def perform(container)
     @container = container
     index = Rails.env + '-' + container
+    Elasticsearch::Model.client.indices.refresh index: index
     records.with_progress("Remapping #{container}").each do |r|
       id = r.key.tr('.json','')
       unless Elasticsearch::Model.client.exists? index: index, type: container, id: id
