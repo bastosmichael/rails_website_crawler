495785180b83fb5b3596f52083ac8681ece93d15
Michael Bastos <bastosmichael@gmail.com>
2015-06-25 09:36:10 -0700

    Updating fixer

--- app/workers/recorder/fixer.rb
+++ app/workers/recorder/fixer.rb
@@ -3,6 +3,17 @@ class Recorder::Fixer < Recorder::Base
     if container && record
       new_hash = Record::Base.new(container, record).data
       new_hash.delete('screenshot')
+
+      if new_hash['price']
+        new_hash['price'] = new_hash['price'].delete_if {|k,v| v.include?('-') }
+        new_hash.delete('price') if new_hash['price'].blank?
+      end
+
+      if new_hash['original_price']
+        new_hash['original_price'] = new_hash['original_price'].delete_if {|k,v| v.include?('-') }
+        new_hash.delete('original_price') if new_hash['original_price'].blank?
+      end
+
       Record::Base.new(container, record).data = new_hash
       Crawler::Slider.perform_async new_hash['url']
     end
