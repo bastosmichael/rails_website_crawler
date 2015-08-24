72f2809a7c2c90e63a01a0887f92db27facbcca7
Michael Bastos <bastosmichael@gmail.com>
2015-08-24 06:46:27 -0700

    One last fix attempt

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,7 @@ workers 1
 # Min and Max threads per worker
 threads 1, 6
 
-app_dir = File.expand_path("../../..", __FILE__)
+app_dir = File.expand_path("../../../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
 # Default to production
