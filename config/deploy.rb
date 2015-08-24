a75a8e03bbae6a8fc04805f3bca909028306b390
Michael Bastos <bastosmichael@gmail.com>
2015-08-24 07:19:59 -0700

    updating deployer

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,23 +5,15 @@ require 'mina/rvm'
 require 'mina_sidekiq/tasks'
 require 'mina/puma'
 
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
-
+set :user, 'ubuntu'
 set :domain, ENV['DOMAIN']
 set :deploy_to, '/home/ubuntu/skynet'
+set :app_path, lambda { "#{deploy_to}/#{current_path}" }
 set :repository, 'git@github.com:bastosmichael/skynet.git'
 set :branch, 'master'
+set :forward_agent, true
 set :rails_env, 'production'
 
-# For system-wide RVM install.
-#   set :rvm_path, '/usr/local/rvm/bin/rvm'
-
-# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
-# They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, ['config/sidekiq.yml',
                     'config/config.yml',
                     'app/sites',
@@ -29,25 +21,10 @@ set :shared_paths, ['config/sidekiq.yml',
                     'tmp/pids',
                     'log']
 
-# Optional settings:
-set :user, 'ubuntu'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
-
-# This task is the environment that is loaded for most commands, such as
-# `mina deploy` or `mina rake`.
 task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
   invoke :'rvm:use[ruby-2.2.2]'
 end
 
-# Put any custom mkdir's in here for when `mina setup` is ran.
-# For Rails apps, we'll make some of the shared paths that are shared between
-# all releases.
 task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
