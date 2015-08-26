be4d6bf4f5336cf567c880c02194d3e5ad11e323
Michael Bastos <bastosmichael@gmail.com>
2015-08-26 05:56:36 -0700

    Deploying puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -82,18 +82,18 @@ namespace :puma do
   desc "Start the application"
   task :start do
     queue 'echo "-----> Start Puma"'
-    queue "cd #{app_path} && RAILS_ENV=#{rails_env} && ~/puma.sh start", :pty => false
+    queue "cd #{app_path} && RAILS_ENV=#{rails_env} && bin/puma.sh start", :pty => false
   end
 
   desc "Stop the application"
   task :stop do
     queue 'echo "-----> Stop Puma"'
-    queue "cd #{app_path} && RAILS_ENV=#{rails_env} && ~/puma.sh stop"
+    queue "cd #{app_path} && RAILS_ENV=#{rails_env} && bin/puma.sh stop"
   end
 
   desc "Restart the application"
   task :restart do
     queue 'echo "-----> Restart Puma"'
-    queue "cd #{app_path} && RAILS_ENV=#{rails_env} && ~/puma.sh restart"
+    queue "cd #{app_path} && RAILS_ENV=#{rails_env} && bin/puma.sh restart"
   end
 end
