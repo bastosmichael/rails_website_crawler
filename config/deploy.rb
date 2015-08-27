a5a5c2466e346088114e7469510124e32093423b
Michael Bastos <bastosmichael@gmail.com>
2015-08-27 06:53:44 -0700

    assets precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,7 +60,7 @@ task :deploy => :environment do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    invoke :'assets:precompile'
+    invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     to :launch do
