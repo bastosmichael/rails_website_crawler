433b855a7848452e46538ef2f9429e91d7f68aa4
Michael Bastos <bastosmichael@gmail.com>
2015-08-30 07:53:19 -0700

    updated clearer

--- app/workers/scheduler/clearer.rb
+++ app/workers/scheduler/clearer.rb
@@ -3,6 +3,11 @@ class Scheduler::Clearer < Scheduler::Base
 
   def perform
     Redis::List.new('sampler_visited').clear
+    Redis::List.new('sampler_one_visited').clear
+    Redis::List.new('sampler_two_visited').clear
+    Redis::List.new('sampler_three_visited').clear
+    Redis::List.new('sampler_four_visited').clear
+    Redis::List.new('sampler_five_visited').clear
     Redis::List.new('spider_visited').clear
     Redis::List.new('spider_one_visited').clear
     Redis::List.new('spider_two_visited').clear
