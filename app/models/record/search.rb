c8c60fbe1a8f3c86715339a02ef7a138311f97ac
Michael Bastos <bastosmichael@gmail.com>
2015-10-21 07:16:31 -0700

    changing search

--- app/models/record/search.rb
+++ app/models/record/search.rb
@@ -8,72 +8,72 @@ class Record::Search < Record::Match
           name: @query_hash[:query]
         }
       },
-      # {
-      #  match: {
-      #    description: @query_hash[:query]
-      #   }
-      # },
-      # {
-      #   match: {
-      #     url: @query_hash[:query]
-      #   }
-      # },
-      # {
-      #   match: {
-      #     tags: @query_hash[:query]
-      #   }
-      # },
-      # {
-      #   match: {
-      #     categories: @query_hash[:query]
-      #   }
-      # },
-      # {
-      #   flt_field: {
-      #     name: {
-      #       like_text: @query_hash[:query],
-      #       analyzer: 'snowball',
-      #       fuzziness: 0.9,
-      #       boost: 5
-      #     }
-      #   }
-      # },
-      # {
-      #   flt_field: {
-      #     url: {
-      #       like_text: @query_hash[:query],
-      #       analyzer: 'snowball',
-      #       fuzziness: 0.7
-      #     }
-      #   }
-      # },
-      # {
-      #   flt_field: {
-      #     tags: {
-      #       like_text: @query_hash[:query],
-      #       analyzer: 'snowball',
-      #       fuzziness: 0.3
-      #     }
-      #   }
-      # },
-      # {
-      #   flt_field: {
-      #     categories: {
-      #       like_text: @query_hash[:query],
-      #       analyzer: 'snowball',
-      #       fuzziness: 0.5
-      #     }
-      #   }
-      # },
-      # {
-      #   flt_field: {
-      #     description: {
-      #       like_text: @query_hash[:query],
-      #       analyzer: 'snowball',
-      #       fuzziness: 0.1
-      #     }
-      #   }
-      # },
+      {
+       match: {
+         description: @query_hash[:query]
+        }
+      },
+      {
+        match: {
+          url: @query_hash[:query]
+        }
+      },
+      {
+        match: {
+          tags: @query_hash[:query]
+        }
+      },
+      {
+        match: {
+          categories: @query_hash[:query]
+        }
+      },
+      {
+        flt_field: {
+          name: {
+            like_text: @query_hash[:query],
+            analyzer: 'snowball',
+            fuzziness: 0.9,
+            boost: 5
+          }
+        }
+      },
+      {
+        flt_field: {
+          url: {
+            like_text: @query_hash[:query],
+            analyzer: 'snowball',
+            fuzziness: 0.7
+          }
+        }
+      },
+      {
+        flt_field: {
+          tags: {
+            like_text: @query_hash[:query],
+            analyzer: 'snowball',
+            fuzziness: 0.3
+          }
+        }
+      },
+      {
+        flt_field: {
+          categories: {
+            like_text: @query_hash[:query],
+            analyzer: 'snowball',
+            fuzziness: 0.5
+          }
+        }
+      },
+      {
+        flt_field: {
+          description: {
+            like_text: @query_hash[:query],
+            analyzer: 'snowball',
+            fuzziness: 0.1
+          }
+        }
+      },
     ]
   end
 end
