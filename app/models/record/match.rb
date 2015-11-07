e4b8bd961b1d66e1c95a3d12d0fc4c57a765baa8
Michael Bastos <bastosmichael@gmail.com>
2015-11-07 07:52:54 -0800

    fixing price

--- app/models/record/match.rb
+++ app/models/record/match.rb
@@ -23,18 +23,21 @@ class Record::Match < Record::Base
     elasticsearch_results[:hits][:hits].map do |e|
       recrawl(e[:_source][:url], @options) if e[:_source][:url]
 
-      new_data = { history: {},
-                   social: {},
-                   id: e[:_id],
+      new_data = { id: e[:_id],
                    container: e[:_type],
-                   score: e[:_score]
+                   score: e[:_score],
+                   history: {},
+                   social: {},
+                   price: {}
                  }
 
       e[:_source].each do |k,v|
         if k.to_s.include?('_history')
-          new_data[:history][k] = v
-        elsif (k.to_s.include?('facebook') || k.to_s.include?('_shares')) && !k.to_s.include?('_history')
+          new_data[:history][k.gsub('_history','')] = v
+        elsif k.to_s.include?('facebook') || k.to_s.include?('_shares')
           new_data[:social][k] = v
+        elsif k.to_s.include?('price')
+          new_data[:price][k] = v
         else
           new_data[k] = v
         end
