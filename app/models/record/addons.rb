5eadbfef5f36ae65a911200cf5cef82f74bbdae9
Michael Bastos <bastosmichael@gmail.com>
2015-11-07 09:03:08 -0800

    fixing addons

--- app/models/record/addons.rb
+++ app/models/record/addons.rb
@@ -2,7 +2,7 @@ class Record::Addons
   def self.insert hash
     if addons = Rails.configuration.config[:admin][:addons][hash[:container].try(:to_sym)]
       addons.each do |key, value|
-        hash[key] = hash[key] + value
+        hash[key] = hash[key] + value if hash[key]
       end
     end
     return hash
