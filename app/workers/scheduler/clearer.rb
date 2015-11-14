640f724e62f3b1de2938083104ebc12219cd4847
Michael Bastos <bastosmichael@gmail.com>
2015-11-13 16:48:44 -0800

    removing clearer for now

--- app/workers/scheduler/clearer.rb
+++ app/workers/scheduler/clearer.rb
@@ -2,17 +2,17 @@ class Scheduler::Clearer < Scheduler::Base
   recurrence { daily }
 
   def perform
-    Redis::List.new('sampler_visited').clear
-    Redis::List.new('sampler_one_visited').clear
-    Redis::List.new('sampler_two_visited').clear
-    Redis::List.new('sampler_three_visited').clear
-    Redis::List.new('sampler_four_visited').clear
-    Redis::List.new('sampler_five_visited').clear
-    Redis::List.new('spider_visited').clear
-    Redis::List.new('spider_one_visited').clear
-    Redis::List.new('spider_two_visited').clear
-    Redis::List.new('spider_three_visited').clear
-    Redis::List.new('spider_four_visited').clear
-    Redis::List.new('spider_five_visited').clear
+    # Redis::List.new('sampler_visited').clear
+    # Redis::List.new('sampler_one_visited').clear
+    # Redis::List.new('sampler_two_visited').clear
+    # Redis::List.new('sampler_three_visited').clear
+    # Redis::List.new('sampler_four_visited').clear
+    # Redis::List.new('sampler_five_visited').clear
+    # Redis::List.new('spider_visited').clear
+    # Redis::List.new('spider_one_visited').clear
+    # Redis::List.new('spider_two_visited').clear
+    # Redis::List.new('spider_three_visited').clear
+    # Redis::List.new('spider_four_visited').clear
+    # Redis::List.new('spider_five_visited').clear
   end
 end
