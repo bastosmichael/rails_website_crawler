2d136cebcc5b6808ca4bae95d6c9854459c5953c
Michael Bastos <bastosmichael@gmail.com>
2015-11-21 09:49:22 -0800

    bringing back fuzzy matching to search

--- app/models/record/search.rb
+++ app/models/record/search.rb
@@ -3,11 +3,11 @@ class Record::Search < Record::Match
 
   def match_query
     [
-      {
-        match: {
-          name: @query_hash[:query]
-        }
-      },
+      # {
+      #   match: {
+      #     name: @query_hash[:query]
+      #   }
+      # },
       # {
       #  match: {
       #    description: @query_hash[:query]
@@ -28,52 +28,52 @@ class Record::Search < Record::Match
       #     categories: @query_hash[:query]
       #   }
       # },
-      # {
-      #   flt_field: {
-      #     name: {
-      #       like_text: @query_hash[:query],
-      #       analyzer: 'snowball',
-      #       fuzziness: 0.9,
-      #       boost: 5
-      #     }
-      #   }
-      # },
-      # {
-      #   flt_field: {
-      #     url: {
-      #       like_text: @query_hash[:query],
-      #       analyzer: 'snowball',
-      #       fuzziness: 0.7
-      #     }
-      #   }
-      # },
-      # {
-      #   flt_field: {
-      #     tags: {
-      #       like_text: @query_hash[:query],
-      #       analyzer: 'snowball',
-      #       fuzziness: 0.3
-      #     }
-      #   }
-      # },
-      # {
-      #   flt_field: {
-      #     categories: {
-      #       like_text: @query_hash[:query],
-      #       analyzer: 'snowball',
-      #       fuzziness: 0.5
-      #     }
-      #   }
-      # },
-      # {
-      #   flt_field: {
-      #     description: {
-      #       like_text: @query_hash[:query],
-      #       analyzer: 'snowball',
-      #       fuzziness: 0.1
-      #     }
-      #   }
-      # },
+      {
+        flt_field: {
+          name: {
+            like_text: @query_hash[:query],
+            analyzer: 'snowball',
+            fuzziness: 0.1,
+            boost: 5
+          }
+        }
+      },
+      {
+        flt_field: {
+          description: {
+            like_text: @query_hash[:query],
+            analyzer: 'snowball',
+            fuzziness: 0.3
+          }
+        }
+      },
+      {
+        flt_field: {
+          url: {
+            like_text: @query_hash[:query],
+            analyzer: 'snowball',
+            fuzziness: 0.5
+          }
+        }
+      },
+      {
+        flt_field: {
+          tags: {
+            like_text: @query_hash[:query],
+            analyzer: 'snowball',
+            fuzziness: 0.7
+          }
+        }
+      },
+      {
+        flt_field: {
+          categories: {
+            like_text: @query_hash[:query],
+            analyzer: 'snowball',
+            fuzziness: 0.9
+          }
+        }
+      },
     ]
   end
 end
