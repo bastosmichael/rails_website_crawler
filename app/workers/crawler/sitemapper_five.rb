2e7e0d3c7ef431c8b1d6b42c59786bd3811ced33
Michael Bastos <bastosmichael@gmail.com>
2015-11-25 11:10:04 -0800

    only crawl

--- app/workers/crawler/sitemapper_five.rb
+++ app/workers/crawler/sitemapper_five.rb
@@ -5,7 +5,7 @@ class Crawler::SitemapperFive < Crawler::Sitemapper
                   unique: :until_executed
 
   def perform(url, type = 'ScrimperFive')
-    if Sidekiq::Queue.new(type.underscore).size <= 50_000
+    if Sidekiq::Queue.new(type.underscore).size <= 100_000
       @url = url
       @type = type
       @name = Page::Url.new(url).name
--- app/workers/crawler/sitemapper_two.rb
+++ app/workers/crawler/sitemapper_two.rb
@@ -5,7 +5,7 @@ class Crawler::SitemapperTwo < Crawler::Sitemapper
                   unique: :until_executed
 
   def perform(url, type = 'ScrimperTwo')
-    if Sidekiq::Queue.new(type.underscore).size <= 50_000
+    if Sidekiq::Queue.new(type.underscore).size <= 100_000
       @url = url
       @type = type
       @name = Page::Url.new(url).name
