f56a4c5bc000db4ac6b69c6dca1446bbe3d5453c
Michael Bastos <bastosmichael@gmail.com>
2015-11-27 06:08:12 -0800

    fixing sitemapper one

--- app/workers/crawler/sitemapper_one.rb
+++ app/workers/crawler/sitemapper_one.rb
@@ -5,7 +5,7 @@ class Crawler::SitemapperOne < Crawler::Sitemapper
                   unique: :until_executed
 
   def perform(url, type = 'ScrimperOne')
-    if Sidekiq::Queue.new(type.underscore).size <= 1_000_000
+    if Sidekiq::Queue.new(type.underscore).size <= 100_000
       @url = url
       @type = type
       @name = Page::Url.new(url).name
