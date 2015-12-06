22bbe527124c33bc092d226c29a1c4e30800373c
Michael Bastos <bastosmichael@gmail.com>
2015-12-06 05:09:49 -0800

    still working on history csv

--- app/controllers/v1/record_controller.rb
+++ app/controllers/v1/record_controller.rb
@@ -12,10 +12,40 @@ class V1::RecordController < V1::AccessController
     respond_to do |format|
       format.json { json_response(200, history) }
       # format.xml { xml_response(200, history) }
-      # format.csv do
-      #   csv_string = history.first.collect { |k, _v| k }.join(',') + "\n" + history.collect { |node| "#{node.collect { |_k, v| v }.join(',')}\n" }.join
-      #   send_data csv_string, type: 'text/csv; charset=iso-8859-1; header=present', disposition: 'attachment;data=historical_data.csv'
-      # end
+      format.csv do
+        # history = Record::Base.new('bestbuy-offers','9071056.json').historical_data
+        headers_hash = history.keys.map {|k| {k => nil}}.inject({},:merge)
+        dates_hash = history.values.flat_map {|hash| if hash.try(:keys) then hash.keys end }.compact.uniq.sort.map {|date| {date.to_date => headers_hash} }.inject({},:merge)
+        dates_hash.each do |key,value|
+          puts key
+          value.each do |k,v|
+            ap k
+            ap v
+            puts history[k][key.to_date]
+          #   # dates_hash[key][k] = history[k][key]
+          end
+        end
+        dates_hash
+
+        # history.each do |key,value|
+        #   if value.is_a? Hash
+        #     ap key
+        #     value.each do |k,v|
+        #       ap k.to_date
+        #       ap v
+        #       dates_hash[k][key] = v
+        #     end
+        #   else
+        #     # dates_hash.keys.each do |date|
+        #     #   dates_hash[date][key] = value
+        #     # end
+        #   end
+        # end
+
+        # dates =
+        # csv_string = history.keys.join(',') + "\n" + history.collect { |node| "#{node.collect { |_k, v| v }.join(',')}\n" }.join
+        # send_data csv_string, type: 'text/csv; charset=iso-8859-1; header=present', disposition: 'attachment;data=historical_data.csv'
+      end
     end
   end
 
