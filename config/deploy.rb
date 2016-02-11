698cb3c7affb1269631a42ff6c2ebcb0b8093394
Michael Bastos <bastosmichael@gmail.com>
2016-02-10 21:05:05 -0800

    fixing deploys with sidekiq

--- config/deploy.rb
+++ config/deploy.rb
@@ -72,7 +72,7 @@ task :deploy => :environment do
 
     to :launch do
       # queue "touch #{deploy_to}/#{shared_path}/pids/sidekiq.pid"
-      # invoke :'sidekiq:restart'
+      invoke :'sidekiq:restart'
       # queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
       invoke :'unicorn:restart'
       # queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
