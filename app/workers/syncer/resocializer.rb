9ac1dedac42bbea6090c92ae2fb56e7a3a49ed87
Michael Bastos <bastosmichael@gmail.com>
2016-02-13 11:05:28 -0800

    fix syncers

--- app/workers/syncer/resocializer.rb
+++ app/workers/syncer/resocializer.rb
@@ -2,7 +2,7 @@ class Syncer::Resocializer < Syncer::Base
   def perform(container)
     @container = container
     records.with_progress("Resocialize Crawling #{container}").each do |r|
-      Crawler::Socializer.perform_async record(r.key.gsub('.json','')).url
+      Crawler::Socializer.perform_async record(r.key.gsub('.json','')).try(:url)
     end
   end
 end
--- app/workers/syncer/respider.rb
+++ app/workers/syncer/respider.rb
@@ -2,7 +2,7 @@ class Syncer::Respider < Syncer::Base
   def perform(container, spider_type = 'Spider')
     @container = container
     records.with_progress("Respider Crawling #{container}").each do |r|
-      ('Crawler::' + spider_type).constantize.perform_async record(r.key.gsub('.json','')).url
+      ('Crawler::' + spider_type).constantize.perform_async record(r.key.gsub('.json','')).try(:url)
     end
   end
 end
