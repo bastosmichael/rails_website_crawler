819e940fa2e6b261425e9fc9e080e4bfae142703
Michael Bastos <bastosmichael@gmail.com>
2016-02-27 12:13:11 -0800

    adding addons

--- app/controllers/v1/links_controller.rb
+++ app/controllers/v1/links_controller.rb
@@ -1,6 +1,9 @@
 class V1::LinksController < V1::AccessController
   def index
     links = Record::Base.new(params[:container], params[:record_id]).links_data(default_options)
+    if links[:links]
+      links[:links] = Record::Addons.insert(links[:links])
+    end
     respond_to do |format|
       format.json { json_response(200, result: links) }
       format.xml { xml_response(200, result: links) }
