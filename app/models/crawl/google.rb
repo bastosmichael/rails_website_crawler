9c6f72ad83d1aa23c6125b753d3d57055c629185
Michael Bastos <bastosmichael@gmail.com>
2016-02-27 09:10:33 -0800

    adding video and book search

---
+++ app/models/crawl/google.rb
@@ -0,0 +1,79 @@
+class Crawl::Google
+  include ActionView::Helpers::DateHelper
+
+  def initialize(query = nil)
+    @query = query
+  end
+
+  def videos
+    @videos ||= if google_hash = Google::Search::Video.new(query: @query).response.hash['responseData']
+                 google_hash['results'].map do |hash|
+                   {
+                    title: hash['titleNoFormatting'],
+                    description: hash['content'],
+                    image: hash['tbUrl'],
+                    url: hash['url'],
+                    length: distance_of_time_in_words(hash['duration'].to_i),
+                    published: hash['published'].to_date.to_s
+                   }
+                 end
+               else
+                 []
+               end
+  end
+
+  def news
+    @news ||= if google_hash = Google::Search::News.new(query: @query).response.hash['responseData']
+                 google_hash['results'].map do |hash|
+                   # {
+                   #  title: hash['titleNoFormatting'],
+                   #  description: hash['content'],
+                   #  image: hash['tbUrl'],
+                   #  url: hash['url'],
+                   #  duration: distance_of_time_in_words(hash['duration'].to_i),
+                   #  published: hash['published'].to_date.to_s
+                   # }
+                 end
+               else
+                 []
+               end
+  end
+
+  def books
+    @books ||= if google_hash = Google::Search::Book.new(query: @query).response.hash['responseData']
+                 google_hash['results'].map do |hash|
+                   description = "by #{hash['authors']} ISBN: #{hash['bookId']}"
+
+                   image = hash['tbUrl'] unless hash['tbUrl'] == "/googlebooks/images/no_cover_thumb.gif"
+
+                   {
+                    title: hash['titleNoFormatting'],
+                    description: description,
+                    image: image,
+                    url: hash['unescapedUrl'],
+                    length: hash['pageCount'] + ' pages',
+                    published: hash['publishedYear']
+                   }
+                 end
+               else
+                 []
+               end
+  end
+
+  def sites
+    @sites ||= if google_hash = Google::Search::Web.new(query: @query).response.hash['responseData']
+                 google_hash['results'].map do |hash|
+                   # {
+                   #  title: hash['titleNoFormatting'],
+                   #  description: hash['content'],
+                   #  image: hash['tbUrl'],
+                   #  url: hash['url'],
+                   #  duration: distance_of_time_in_words(hash['duration'].to_i),
+                   #  published: hash['published'].to_date.to_s
+                   # }
+                 end
+               else
+                 []
+               end
+  end
+end
