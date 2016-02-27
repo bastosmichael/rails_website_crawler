7b8e50456d0f2f713bf6ae4d1eed7e75d4398a6c
Michael Bastos <bastosmichael@gmail.com>
2016-02-27 12:16:34 -0800

    fixed links controller

--- app/controllers/v1/links_controller.rb
+++ app/controllers/v1/links_controller.rb
@@ -2,7 +2,7 @@ class V1::LinksController < V1::AccessController
   def index
     links = Record::Base.new(params[:container], params[:record_id]).links_data(default_options)
     if links[:links]
-      links[:links] = Record::Addons.insert(links[:links])
+      links[:links] = links[:links].map {|h| Record::Addons.insert(h) }
     end
     respond_to do |format|
       format.json { json_response(200, result: links) }
