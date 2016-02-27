8369aa50218451042186108f60d3b6e7a579c1f3
Michael Bastos <bastosmichael@gmail.com>
2016-02-27 10:18:00 -0800

    finihsed google model

--- app/models/crawl/google.rb
+++ app/models/crawl/google.rb
@@ -23,19 +23,33 @@ class Crawl::Google
 
   def news
     @news ||= if google_hash = Google::Search::News.new(query: @query).response.hash['responseData']
-                 google_hash['results'].map do |hash|
-                   # {
-                   #  title: hash['titleNoFormatting'],
-                   #  description: hash['content'],
-                   #  image: hash['tbUrl'],
-                   #  url: hash['url'],
-                   #  duration: distance_of_time_in_words(hash['duration'].to_i),
-                   #  published: hash['published'].to_date.to_s
-                   # }
-                 end
-               else
-                 []
-               end
+                google_hash['results'].map do |hash|
+                  image = hash['image']['tbUrl'] if hash['image']
+                  if related = hash['relatedStories']
+                    related = related.map do |h|
+                      {
+                        title: h['titleNoFormatting'],
+                        url: h['unescapedUrl'],
+                        publisher: h['publisher'],
+                        published: h['publishedDate'].to_date.to_s,
+                        language: hash['language']
+                      }
+                    end
+                  end
+                  {
+                    title: hash['titleNoFormatting'],
+                    description: hash['content'],
+                    image: image,
+                    url: hash['unescapedUrl'],
+                    publisher: hash['publisher'],
+                    published: hash['publishedDate'].to_date.to_s,
+                    language: hash['language'],
+                    related: related || []
+                  }
+                end
+              else
+                []
+              end
   end
 
   def books
