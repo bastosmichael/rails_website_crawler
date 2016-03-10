15f04fd6fcb0ed41e2c3cfab4cf8690ba78a10d7
Michael Bastos <bastosmichael@gmail.com>
2016-03-09 17:13:25 -0800

    caching some stuff

--- app/models/crawl/google.rb
+++ app/models/crawl/google.rb
@@ -6,85 +6,93 @@ class Crawl::Google
   end
 
   def videos
-    @videos ||= if google_hash = Google::Search::Video.new(query: @query).response.hash['responseData']
-                 google_hash['results'].map do |hash|
-                   {
-                    title: hash['titleNoFormatting'],
-                    description: ActionView::Base.full_sanitizer.sanitize(hash['content']),
-                    image: hash['tbUrl'],
-                    url: hash['url'],
-                    length: distance_of_time_in_words(hash['duration'].to_i),
-                    published: hash['published'].to_date.to_s
-                   }
-                 end
-               else
-                 nil
-               end
+    @videos ||= Rails.cache.fetch("#{@query}/google_videos", expires_in: 7.days) do
+      if google_hash = Google::Search::Video.new(query: @query).response.hash['responseData']
+        google_hash['results'].map do |hash|
+          {
+            title: hash['titleNoFormatting'],
+            description: ActionView::Base.full_sanitizer.sanitize(hash['content']),
+            image: hash['tbUrl'],
+            url: hash['url'],
+            length: distance_of_time_in_words(hash['duration'].to_i),
+            published: hash['published'].to_date.to_s
+          }
+        end
+      else
+        nil
+      end
+    end
   end
 
   def news
-    @news ||= if google_hash = Google::Search::News.new(query: @query).response.hash['responseData']
-                google_hash['results'].map do |hash|
-                  image = hash['image']['tbUrl'] if hash['image']
-                  if related = hash['relatedStories']
-                    related = related.map do |h|
-                      {
-                        title: h['titleNoFormatting'],
-                        url: h['unescapedUrl'],
-                        publisher: h['publisher'],
-                        published: h['publishedDate'].to_date.to_s,
-                        language: hash['language']
-                      }
-                    end
-                  end
-                  {
-                    title: hash['titleNoFormatting'],
-                    description: ActionView::Base.full_sanitizer.sanitize(hash['content']),
-                    image: image,
-                    url: hash['unescapedUrl'],
-                    publisher: hash['publisher'],
-                    published: hash['publishedDate'].to_date.to_s,
-                    language: hash['language'],
-                    related: related || []
-                  }
-                end
-              else
-                nil
-              end
+    @news ||= Rails.cache.fetch("#{@query}/google_news", expires_in: 7.days) do
+      if google_hash = Google::Search::News.new(query: @query).response.hash['responseData']
+        google_hash['results'].map do |hash|
+          image = hash['image']['tbUrl'] if hash['image']
+          if related = hash['relatedStories']
+            related = related.map do |h|
+              {
+                title: h['titleNoFormatting'],
+                url: h['unescapedUrl'],
+                publisher: h['publisher'],
+                published: h['publishedDate'].to_date.to_s,
+                language: hash['language']
+              }
+            end
+          end
+          {
+            title: hash['titleNoFormatting'],
+            description: ActionView::Base.full_sanitizer.sanitize(hash['content']),
+            image: image,
+            url: hash['unescapedUrl'],
+            publisher: hash['publisher'],
+            published: hash['publishedDate'].to_date.to_s,
+            language: hash['language'],
+            related: related || []
+          }
+        end
+      else
+        nil
+      end
+    end
   end
 
   def references
-    @references ||= if google_hash = Google::Search::Book.new(query: @query).response.hash['responseData']
-                 google_hash['results'].map do |hash|
-                   description = "by #{hash['authors']} ISBN: #{hash['bookId']}"
+    @references ||= Rails.cache.fetch("#{@query}/google_references", expires_in: 7.days) do
+      if google_hash = Google::Search::Book.new(query: @query).response.hash['responseData']
+        google_hash['results'].map do |hash|
+          description = "by #{hash['authors']} ISBN: #{hash['bookId']}"
 
-                   image = hash['tbUrl'] unless hash['tbUrl'] == "/googlebooks/images/no_cover_thumb.gif"
+          image = hash['tbUrl'] unless hash['tbUrl'] == "/googlebooks/images/no_cover_thumb.gif"
 
-                   {
-                    title: hash['titleNoFormatting'],
-                    description: description,
-                    image: image,
-                    url: hash['unescapedUrl'],
-                    length: hash['pageCount'] + ' pages',
-                    published: hash['publishedYear']
-                   }
-                 end
-               else
-                 nil
-               end
+          {
+            title: hash['titleNoFormatting'],
+            description: description,
+            image: image,
+            url: hash['unescapedUrl'],
+            length: hash['pageCount'] + ' pages',
+            published: hash['publishedYear']
+          }
+        end
+      else
+        nil
+      end
+    end
   end
 
   def links
-    @links ||= if google_hash = Google::Search::Web.new(query: @query).response.hash['responseData']
-                 google_hash['results'].map do |hash|
-                   {
-                    title: hash['titleNoFormatting'],
-                    description: ActionView::Base.full_sanitizer.sanitize(hash['content']),
-                    url: hash['unescapedUrl']
-                   }
-                 end
-               else
-                 nil
-               end
+    @links ||= Rails.cache.fetch("#{@query}/google_links", expires_in: 7.days) do
+      if google_hash = Google::Search::Web.new(query: @query).response.hash['responseData']
+        google_hash['results'].map do |hash|
+          {
+            title: hash['titleNoFormatting'],
+            description: ActionView::Base.full_sanitizer.sanitize(hash['content']),
+            url: hash['unescapedUrl']
+          }
+        end
+      else
+        nil
+      end
+    end
   end
 end
