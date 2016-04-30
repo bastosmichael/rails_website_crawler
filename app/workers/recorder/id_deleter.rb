42abaec197deb4aacafd9064d4a3e59d06eb8081
Michael Bastos <bastosmichael@gmail.com>
2016-04-30 10:21:33 -0700

    turning off id_deleter

--- app/workers/recorder/id_deleter.rb
+++ app/workers/recorder/id_deleter.rb
@@ -3,8 +3,8 @@ class Recorder::IdDeleter < Recorder::Base
     @container = container
     types = @container.split('-').last.pluralize.gsub(':', '')
     @index = Rails.env + '-' + types
-    cloud.head(id + '.json').try(:destroy)
-    Elasticsearch::Model.client.delete index: @index, type: @container, id: id
-    Elasticsearch::Model.client.indices.refresh index: @index
+    # cloud.head(id + '.json').try(:destroy)
+    # Elasticsearch::Model.client.delete index: @index, type: @container, id: id
+    # Elasticsearch::Model.client.indices.refresh index: @index
   end
 end
