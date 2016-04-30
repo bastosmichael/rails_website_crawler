ab1f9b30a79f0ab7266ae87a8bed9e6f69fa28a1
Michael Bastos <bastosmichael@gmail.com>
2016-04-30 10:10:42 -0700

    changing url deleter

--- app/workers/recorder/url_deleter.rb
+++ app/workers/recorder/url_deleter.rb
@@ -5,11 +5,14 @@ class Recorder::UrlDeleter < Recorder::Base
     types = @container.split('-').last.pluralize.gsub(':', '')
     @index = Rails.env + '-' + types
 
-    record = Elasticsearch::Model.client.search(index: @index, type: @container, body: { query: { match_phrase_prefix: { url: url } } })
+    records = Elasticsearch::Model.client.search(index: @index, type: @container, body: { query: { match_phrase_prefix: { url: url } } })
 
-    if record['hits']['total'] > 0
-      Elasticsearch::Model.client.delete index: @index, type: @container, id: record['hits']['hits'].try(:first)['_id']
-      cloud.head(record['hits']['hits'].try(:first)['_id'] + '.json').try(:destroy)
+    if records['hits']['total'] > 0
+      records['hits']['hits'].each do |record|
+        Recorder::Uploader.perform_async { id: record['_id'], available: false, url: record['_source']['url'], record['_type'].split('-').last.capitalize.singularize }
+      end
+      # Elasticsearch::Model.client.delete index: @index, type: @container, id: record['hits']['hits'].try(:first)['_id']
+      # cloud.head(record['hits']['hits'].try(:first)['_id'] + '.json').try(:destroy)
     end
   end
 end
