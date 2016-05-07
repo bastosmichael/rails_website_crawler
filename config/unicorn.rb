6be4a929334f80ed55f9eba594710f29223233ec
Michael Bastos <bastosmichael@gmail.com>
2016-05-07 08:51:47 -0700

    reducing unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@ stderr_path 'log/unicorn.stderr.log'
 stdout_path 'log/unicorn.stdout.log'
 
 # workers
-worker_processes 4
+worker_processes 2
 
 # use correct Gemfile on restarts
 before_exec do |_server|
