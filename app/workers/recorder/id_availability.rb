4853cd45c5ce7d247fe3e7ff6680566208f594da
Michael Bastos <bastosmichael@gmail.com>
2016-07-11 05:18:58 -0700

    fixing deleter

--- app/workers/recorder/id_availability.rb
+++ app/workers/recorder/id_availability.rb
@@ -1,10 +1,9 @@
 class Recorder::IdAvailability < Recorder::Base
   def perform(container, id)
-    @container = container
-    types = @container.split('-').last.pluralize.gsub(':', '')
-    @index = Rails.env + '-' + types
-    # cloud.head(id + '.json').try(:destroy)
-    # Elasticsearch::Model.client.delete index: @index, type: @container, id: id
-    # Elasticsearch::Model.client.indices.refresh index: @index
+    types = container.split('-').last.pluralize.gsub(':', '')
+    index = Rails.env + '-' + types
+    cloud.head(id + '.json').try(:destroy)
+    Elasticsearch::Model.client.delete index: index, type: container, id: id
+    Elasticsearch::Model.client.indices.refresh index: index
   end
 end
