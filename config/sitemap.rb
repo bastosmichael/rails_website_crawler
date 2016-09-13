8c574d4e275f00439cb6f2b19e894c5771353fef
Michael Bastos <bastosmichael@gmail.com>
2016-09-13 11:09:09 -0700

    updating sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,16 +5,23 @@ SitemapGenerator::Sitemap.sitemaps_path = "#{ENV['CONTAINER']}/"
 
 SitemapGenerator::Sitemap.create_index = true
 
+SitemapGenerator::Sitemap.public_path = "tmp/#{ENV['DOMAIN']}/#{ENV['CONTAINER']}/"
+
 SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['DOMAIN']}/"
 
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(Rails.configuration.config[:fog].merge(fog_directory: "#{ENV['DOMAIN']}-sitemaps",
                                          fog_region: 'us-west-1', fog_provider: 'AWS'))
+sitemap_default_options = {
+  changefreq: nil,
+  priority: nil,
+  lastmod: nil
+}
 
 SitemapGenerator::Sitemap.create do
   # ['boxed-offers'].each do |container|
   begin
     Cloud.new(ENV['CONTAINER']).files.each do |file|
-      add (ENV['CONTAINER'] + '/' + file.key.gsub('.json',''))#, lastmod: file.last_modified
+      add (ENV['CONTAINER'] + '/' + file.key.gsub('.json','')), sitemap_default_options#.merge(lastmod: file.last_modified)
     end
   rescue => e
     ap e.message
