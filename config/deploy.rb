6708d494bf89fe77ec450d69f8730ba837d9bc69
Michael Bastos <bastosmichael@gmail.com>
2017-02-09 23:18:28 -0800

    fixed deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,15 +17,17 @@ set :keep_releases, 5
 set :sidekiq_log, "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/log/sidekiq.log"
 set :sidekiq_pid, "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/tmp/pids/sidekiq.pid"
 
-set :shared_paths, ['public/static',
-                    'public/sitemaps',
-                    'config/sidekiq.yml',
-                    'config/config.yml',
-                    'config/secrets.yml',
-                    'app/sites',
-                    'tmp/sockets',
-                    'tmp/pids',
-                    'log']
+set :shared_dirs, fetch(:shared_dirs, []).push( 'public/static',
+                                                'public/sitemaps',
+                                                'app/sites',
+                                                'tmp/sockets',
+                                                'tmp/pids',
+                                                'log' )
+
+
+set :shared_files, fetch(:shared_files, []).push( 'config/sidekiq.yml',
+                                                  'config/config.yml',
+                                                  'config/secrets.yml' )
 
 task :environment do
   invoke :chruby, 'ruby-2.3.0'
@@ -80,3 +82,75 @@ task :deploy => :environment do
     end
   end
 end
+
+# require 'mina/rails'
+# require 'mina/git'
+# # require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
+# # require 'mina/rvm'    # for rvm support. (https://rvm.io)
+
+# # Basic settings:
+# #   domain       - The hostname to SSH to.
+# #   deploy_to    - Path to deploy into.
+# #   repository   - Git repo to clone from. (needed by mina/git)
+# #   branch       - Branch name to deploy. (needed by mina/git)
+
+# set :application_name, 'foobar'
+# set :domain, 'foobar.com'
+# set :deploy_to, '/var/www/foobar.com'
+# set :repository, 'git://...'
+# set :branch, 'master'
+
+# # Optional settings:
+# #   set :user, 'foobar'          # Username in the server to SSH to.
+# #   set :port, '30000'           # SSH port number.
+# #   set :forward_agent, true     # SSH forward_agent.
+
+# # shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
+# # set :shared_dirs, fetch(:shared_dirs, []).push('somedir')
+# # set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# # This task is the environment that is loaded for all remote run commands, such as
+# # `mina deploy` or `mina rake`.
+# task :environment do
+#   # If you're using rbenv, use this to load the rbenv environment.
+#   # Be sure to commit your .ruby-version or .rbenv-version to your repository.
+#   # invoke :'rbenv:load'
+
+#   # For those using RVM, use this to load an RVM version@gemset.
+#   # invoke :'rvm:use', 'ruby-1.9.3-p125@default'
+# end
+
+# # Put any custom commands you need to run at setup
+# # All paths in `shared_dirs` and `shared_paths` will be created on their own.
+# task :setup do
+#   # command %{rbenv install 2.3.0}
+# end
+
+# desc "Deploys the current version to the server."
+# task :deploy do
+#   # uncomment this line to make sure you pushed your local branch to the remote origin
+#   # invoke :'git:ensure_pushed'
+#   deploy do
+#     # Put things that will set up an empty directory into a fully set-up
+#     # instance of your project.
+#     invoke :'git:clone'
+#     invoke :'deploy:link_shared_paths'
+#     invoke :'bundle:install'
+#     invoke :'rails:db_migrate'
+#     invoke :'rails:assets_precompile'
+#     invoke :'deploy:cleanup'
+
+#     on :launch do
+#       in_path(fetch(:current_path)) do
+#         command %{mkdir -p tmp/}
+#         command %{touch tmp/restart.txt}
+#       end
+#     end
+#   end
+
+#   # you can use `run :local` to run tasks on local machine before of after the deploy scripts
+#   # run(:local){ say 'done' }
+# end
+# # For help in making your deploy script, see the Mina documentation:
+# #
+# #  - https://github.com/mina-deploy/mina/tree/master/docs
