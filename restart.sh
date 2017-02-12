902f9cb7ffae7ef2c9a090c9af55d7e73d290024
Michael Bastos <bastosmichael@gmail.com>
2017-02-11 22:26:19 -0800

    fixing restart

---
+++ restart.sh
@@ -0,0 +1,1 @@
+"chruby 2.3.0 && git checkout . && git pull origin master && rm .ruby-version && cd app/sites && git pull origin master && cd ../.. && cp config/sidekiq.yml.example config/sidekiq.yml && bundle && RAILS_ENV=production bundle exec sidekiq -d -L log/sidekiq.log
