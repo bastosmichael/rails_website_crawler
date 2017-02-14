f7983e3ac973fc8f564fd0cb6452b52a2afddccb
Michael Bastos <bastosmichael@gmail.com>
2017-02-14 11:30:13 -0800

    adding timeout back in

--- app/models/crawl/base.rb
+++ app/models/crawl/base.rb
@@ -54,10 +54,10 @@ class Crawl::Base < Page::Url
     agent.html_parser = Nokogiri::HTML
     agent.redirect_ok = false
     # agent.ssl_version = 'SSLv3'
-    # agent.open_timeout = 300
-    # agent.read_timeout = 300
-    # agent.idle_timeout = 300
-    # agent.max_history = 10
+    agent.open_timeout = 300
+    agent.read_timeout = 300
+    agent.idle_timeout = 300
+    agent.max_history = 10
     agent.keep_alive = false
     agent
   end
