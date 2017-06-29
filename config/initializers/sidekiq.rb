1200e9c3a4e8c7a1396198029a003043a24abe35
Michael Bastos <bastosmichael@gmail.com>
2017-06-28 17:03:16 -0700

    fixing sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,23 @@
 require 'sidekiq'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://:#{Rails.configuration.config[:redis][:password]}@#{Rails.configuration.config[:redis][:host]}:#{Rails.configuration.config[:redis][:port]}/#{Rails.configuration.config[:redis][:database]}", namespace: 'crawler' }
+  if Rails.configuration.config[:redis][:password].presence
+    url = "redis://:#{Rails.configuration.config[:redis][:password]}@#{Rails.configuration.config[:redis][:host]}:#{Rails.configuration.config[:redis][:port]}/#{Rails.configuration.config[:redis][:database]}"
+  else
+    url = "redis://#{Rails.configuration.config[:redis][:host]}:#{Rails.configuration.config[:redis][:port]}/#{Rails.configuration.config[:redis][:database]}"
+  end
+
+  config.redis = { url: url, namespace: 'crawler' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://:#{Rails.configuration.config[:redis][:password]}@#{Rails.configuration.config[:redis][:host]}:#{Rails.configuration.config[:redis][:port]}/#{Rails.configuration.config[:redis][:database]}", namespace: 'crawler' }
+  if Rails.configuration.config[:redis][:password].presence
+    url = "redis://:#{Rails.configuration.config[:redis][:password]}@#{Rails.configuration.config[:redis][:host]}:#{Rails.configuration.config[:redis][:port]}/#{Rails.configuration.config[:redis][:database]}"
+  else
+    url = "redis://#{Rails.configuration.config[:redis][:host]}:#{Rails.configuration.config[:redis][:port]}/#{Rails.configuration.config[:redis][:database]}"
+  end
+
+  config.redis = { url: url, namespace: 'crawler' }
 end
 
 SidekiqUniqueJobs.config.unique_args_enabled
