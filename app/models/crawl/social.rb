ea87379fbdd24b2bb7db608c764476e9b6fd84a5
Michael Bastos <bastosmichael@gmail.com>
2017-07-06 17:26:51 -0700

    fixing social

--- app/models/crawl/social.rb
+++ app/models/crawl/social.rb
@@ -2,11 +2,17 @@ class Crawl::Social < Page::Url
   require 'social_shares'
 
   def shares
-    all.delete_if { |_k, v| v == 0 }
+    if shares = all(url).delete_if { |_k, v| v == 0 }.presence
+      return shares
+    elsif url.starts_with?('https') && shares = all(url.gsub('https://','http://')).delete_if { |_k, v| v == 0 }.presence
+      return shares
+    else
+      {}
+    end
   end
 
-  def all
-    @all ||= SocialShares.all(url).delete_if { |_k, v| v == 0 }.map { |k, v| { k.to_s + '_shares' => v.to_i } }.reduce({}, :merge)
+  def all(new_url)
+    @all ||= SocialShares.all(new_url).delete_if { |_k, v| v == 0 }.map { |k, v| { k.to_s + '_shares' => v.to_i } }.reduce({}, :merge)
   end
 
   def total
@@ -18,7 +24,7 @@ class Crawl::Social < Page::Url
   end
 
   def facebook
-    @facebook ||= sanitize_facebook JSON.parse(Crawl::Base.new("http://graph.facebook.com/?id=#{@url}").get.try(:body), quirks_mode: true)
+    @facebook ||= sanitize_facebook JSON.parse(Crawl::Base.new("https://graph.facebook.com/?id=#{@url}").get.try(:body), quirks_mode: true)
   rescue
     {}
   end
